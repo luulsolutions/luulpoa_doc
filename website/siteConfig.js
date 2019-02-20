2b519ebe3cc7cc23843ff8c1339d45596b60eb23
Mohamed Elmi <maxto024@gmail.com>
2019-02-20 13:45:51 +0300

    Fixes some typos

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'LuulPOS', // Title for your website.
   tagline: 'Open Source Point Of Sale',
-  url: 'http://luulpos.org', // Your website URL
+  url: 'https://luulpos.org', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
